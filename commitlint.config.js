2df2817944d717e6153ef4a4c597bdadfca959f5
ImJustNon <Im_Just_Non@hotmail.com>
2025-12-23 12:51:48 +0700

    refactor: commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default { 
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "setup",
+        "config",
+        "deps",
+        "feature",
+        "bug",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "build",
+        "ci",
+        "release",
+        "other",
+      ],
+    ],
+  },
+};
