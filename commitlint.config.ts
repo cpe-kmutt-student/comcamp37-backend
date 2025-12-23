ad51dc1ee6df57e9d7a7fa7c91f7b8ea691eef22
ImJustNon <Im_Just_Non@hotmail.com>
2025-12-23 12:56:58 +0700

    fix: new commitlint config and readme

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,25 +2,4 @@ import type { UserConfig } from "@commitlint/types";
 
 export default {
 	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"scope-enum": [
-			2,
-			"always",
-			[
-				"setup",
-				"config",
-				"deps",
-				"feature",
-				"bug",
-				"docs",
-				"style",
-				"refactor",
-				"test",
-				"build",
-				"ci",
-				"release",
-				"other",
-			],
-		],
-	},
 } satisfies UserConfig;
