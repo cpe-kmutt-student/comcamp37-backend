5ba27bd43fb4bfee9a7f1902e6e1afd3fbdb4257
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-01 00:30:43 +0700

    refactor: test add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
+// import { UsersModule } from "./users/users.module";
 import { PrismaService } from "./prisma/prisma.service";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./prisma/prisma.module";
@@ -9,7 +9,7 @@ import { UploadService } from "./upload/upload.service";
 import { UploadModule } from "./upload/upload.module";
 
 @Module({
-	imports: [UsersModule, ConfigModule.forRoot(), PrismaModule, UploadModule],
+	imports: [ConfigModule.forRoot(), PrismaModule, UploadModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService, UploadService],
 })
