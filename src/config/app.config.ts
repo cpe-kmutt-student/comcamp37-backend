252e2bf7695b785ab8925ac63ba1bc493800f071
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-05 02:14:05 +0700

    feat: update redicrect config google and frontend

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,6 +5,7 @@ export const config = {
 	app: {
 		port: parseInt(process.env.APP_PORT || "3000", 10),
 		allowOrigins: (process.env.APP_ALLOW_ORIGIN || "").split(","),
+		frontendUrl: process.env.APP_FRONTEND_URL || "",
 	},
 	db: {
 		url: process.env.DATABASE_URL || "",
@@ -20,5 +21,6 @@ export const config = {
 		jwtSecret: process.env.AUTH_JWT_SECRET || "KEYES",
 		googleClientId: process.env.AUTH_GOOGLE_CLIENT_ID || "",
 		googleClientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET || "",
+		googleCallbackUrl: process.env.AUTH_GOOGLE_CALLBACK_URL || "http://localhost:3030/student/google/callback",
 	},
 };
