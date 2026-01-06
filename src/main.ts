5b1ce0e5d61658893e54b877c9b2c3948f3670f3
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-06 23:59:18 +0700

    refactor: middleware config

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,26 @@ import bodyParser from "body-parser";
 import { corsConfig } from "./config/cors.config";
 import cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
+import type { IncomingMessage, ServerResponse } from "node:http";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
 		bodyParser: false,
 	});
 
-	// app.enableCors(corsConfig);
-	app.enableCors({
-		origin: "http://localhost:3030",
-		credentials: true,
-	});
-	app.use(cookieParser());
-	// app.use(
-	// 	bodyParser.urlencoded({
-	// 		extended: false,
-	// 		limit: "10mb",
-	// 	}),
-	// );
-	app.use(morgan("dev"));
-
+	app.enableCors(corsConfig);
 	app.useGlobalPipes(new ValidationPipe());
+	app.use(cookieParser());
+	app.use(morgan("dev"));
+	
+
+	// const expressApp = app.getHttpAdapter().getInstance();
+	// const authHandler = toNodeHandler(auth);
+	// expressApp.all("/api/auth/*path", (req: IncomingMessage, res: ServerResponse) => {
+	// 	return authHandler(req, res);
+	// });
 
 	await app.listen(config.app.port);
 }
