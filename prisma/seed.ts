bbf123da94d9fe93158fb262c4c8cd59ed395eae
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-07 23:04:47 +0700

    feat: remove old seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,20 +14,20 @@ class PrismaSeed {
 	}
 
 	async createAdminRole() {
-		const cRole = await prisma.staffRoles.createMany({
-			data: [
-				{
-					stf_role_name: "Admin",
-					stf_role_weight: 0,
-				},
-				{
-					stf_role_name: "Staff",
-					stf_role_weight: 1,
-				},
-			],
-		});
+		// const cRole = await prisma.staffRoles.createMany({
+		// 	data: [
+		// 		{
+		// 			stf_role_name: "Admin",
+		// 			stf_role_weight: 0,
+		// 		},
+		// 		{
+		// 			stf_role_name: "Staff",
+		// 			stf_role_weight: 1,
+		// 		},
+		// 	],
+		// });
 
-		console.log(cRole);
+		// console.log(cRole);
 	}
 
 	async disconnect() {
