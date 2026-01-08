847b332eab34fdffed4b28d27e04754f8486dfc3
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-09 04:31:10 +0700

    feat: change Method for GET purpose

--- src/student-user/student-user.controller.ts
+++ src/student-user/student-user.controller.ts
@@ -1,35 +1,35 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
-import { StudentUserService } from "./student-user.service";
-import type { Request, Response } from "express";
-import { Session } from "@thallesp/nestjs-better-auth";
 import type { UserSession } from "@thallesp/nestjs-better-auth";
+import { Session } from "@thallesp/nestjs-better-auth";
+import type { Request, Response } from "express";
+import { StudentUserService } from "./student-user.service";
 
 @Controller("student/user")
 export class StudentUserController {
 	constructor(private readonly StudentUserService: StudentUserService) {}
 
-	@Post("/me")
-	getMe(@Session() session: UserSession, @Req() req: Request) {
+	@Get("/me")
+	getMe(@Session() session: UserSession) {
 		return this.StudentUserService.getMe(session.user.id);
 	}
 
-	@Post("/info")
-	getDetails(@Session() session: UserSession, @Req() req: Request) {
+	@Get("/info")
+	getDetails(@Session() session: UserSession) {
 		return this.StudentUserService.getInfo(session.user.id);
 	}
 
-	@Post("/status")
-	getStatus(@Session() session: UserSession, @Req() req: Request) {
+	@Get("/status")
+	getStatus(@Session() session: UserSession) {
 		return this.StudentUserService.getStatus(session.user.id);
 	}
 
-	@Post("/questions")
-	getQuestion(@Session() session: UserSession, @Req() req: Request) {
+	@Get("/questions")
+	getQuestion(@Session() session: UserSession) {
 		return this.StudentUserService.getQuestions(session.user.id);
 	}
 
-	@Post("/files")
-	getFiles(@Session() session: UserSession, @Req() req: Request) {
+	@Get("/files")
+	getFiles(@Session() session: UserSession) {
 		return this.StudentUserService.getFiles(session.user.id);
 	}
 }
