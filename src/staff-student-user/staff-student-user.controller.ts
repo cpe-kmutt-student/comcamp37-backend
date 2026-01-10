a7e92223b180d28a440f37c0b029b036e0046e40
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-11 02:18:14 +0700

    feat: add staff student info edit service

--- src/staff-student-user/staff-student-user.controller.ts
+++ src/staff-student-user/staff-student-user.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import type { UserSession } from "@thallesp/nestjs-better-auth";
 import { Session } from "@thallesp/nestjs-better-auth";
 import { StaffGuard } from "src/staff/staff.guard";
-import { StaffStudentGetByIdDto } from "./dto/staff-student-user.dto";
+import { StaffStudentGetByIdDto, StaffStudentInfoEditDto } from "./dto/staff-student-user.dto";
 import { StaffStudentUserService } from "./staff-student-user.service";
 
 @Controller("/api/staff/student/user")
@@ -19,5 +19,11 @@ export class StaffStudentUserController {
 	@UseGuards(StaffGuard)
 	getStudentById(@Param() staffStudentGetByIdDto: StaffStudentGetByIdDto) {
 		return this.staffStudentUserService.getStudentById(staffStudentGetByIdDto.id);
+	}
+
+	@Post("/info/edit")
+	@UseGuards(StaffGuard)
+	studentInfoEdit(@Body() staffStudentInfoEditDto: StaffStudentInfoEditDto) {
+		return this.staffStudentUserService.studentInfoEdit(staffStudentInfoEditDto);
 	}
 }
--- src/staff-student-user/staff-student-user.service.ts
+++ src/staff-student-user/staff-student-user.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { StaffStudentInfoEditDto } from "./dto/staff-student-user.dto";
 
 @Injectable()
 export class StaffStudentUserService {
@@ -32,5 +33,61 @@ export class StaffStudentUserService {
 				user_status: true,
 			},
 		});
+	}
+
+	async studentInfoEdit(staffStudentInfoEditDto: StaffStudentInfoEditDto) {
+		try {
+			const studentInfo = await this.prisma.studentInfo.findUnique({
+				where: {
+					std_user_id: staffStudentInfoEditDto.id,
+					std_user: {
+						role: "user",
+					},
+				},
+			});
+
+			if (!studentInfo) {
+				return new NotFoundException().getResponse();
+			}
+
+			return await this.prisma.studentInfo.update({
+				where: {
+					std_user_id: staffStudentInfoEditDto.id,
+					std_user: {
+						role: "user",
+					},
+				},
+				data: {
+					std_user_nick_name: staffStudentInfoEditDto.nick_name,
+					std_user_first_name: staffStudentInfoEditDto.first_name,
+					std_user_last_name: staffStudentInfoEditDto.last_name,
+					std_info_age: staffStudentInfoEditDto.age,
+					std_info_gender: staffStudentInfoEditDto.gender,
+					std_info_religion: staffStudentInfoEditDto.religion,
+					std_info_blood_group: staffStudentInfoEditDto.blood_group,
+					std_info_education_level: staffStudentInfoEditDto.education_level,
+					std_info_education_plan: staffStudentInfoEditDto.education_plan,
+					std_info_phone: staffStudentInfoEditDto.phone,
+					std_info_alternative_email: staffStudentInfoEditDto.alternative_email,
+					std_info_medical_insurance: staffStudentInfoEditDto.medical_insurance,
+					std_info_chronic_disease: staffStudentInfoEditDto.chronic_disease,
+					std_info_drug_allergy: staffStudentInfoEditDto.drug_allergy,
+					std_info_food_allergy: staffStudentInfoEditDto.food_allergy,
+					std_info_address: staffStudentInfoEditDto.address,
+					std_info_home_phone: staffStudentInfoEditDto.home_phone,
+					std_info_have_participated: staffStudentInfoEditDto.have_participated,
+					std_info_shirt_size: staffStudentInfoEditDto.shirt_size,
+					std_info_have_laptop: staffStudentInfoEditDto.have_laptop,
+					std_info_travel_plan: staffStudentInfoEditDto.travel_plan,
+					std_info_parent_name: staffStudentInfoEditDto.parent_name,
+					std_info_parent_relation: staffStudentInfoEditDto.parent_relation,
+					std_info_parent_phone: staffStudentInfoEditDto.parent_phone,
+					std_info_food_prefer: staffStudentInfoEditDto.food_prefer,
+					std_info_can_participate_every_day: staffStudentInfoEditDto.can_participate_every_day,
+				},
+			});
+		} catch (e) {
+			throw new InternalServerErrorException();
+		}
 	}
 }
