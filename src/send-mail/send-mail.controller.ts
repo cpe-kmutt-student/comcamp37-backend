7f9f7768362bdbcab1bebbcc1406108695d1fb9f
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-20 03:19:15 +0700

    test: use resend for send mail

--- src/send-mail/send-mail.controller.ts
+++ src/send-mail/send-mail.controller.ts
@@ -1,19 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { SendMailService } from "./send-mail.service";
 
 @Controller("/api/send-mail")
 export class SendMailController {
+	constructor(private readonly sendMailService: SendMailService) {}
+
 	@Get("/test")
 	async sendTest() {
-		console.log(
-			await resend.emails.send({
-				from: `no-reply@send.mail.comcamp.io`,
-				to: ["Im_Just_Non@hotmail.com"],
-				subject: "title",
-				react: "asd",
-			}),
-		);
+		this.sendMailService.sendTest();
 	}
 }
