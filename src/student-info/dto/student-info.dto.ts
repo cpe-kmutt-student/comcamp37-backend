8dfc98ebea0f8336a67d6ae427824d0e83ae1cea
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-22 14:26:15 +0700

    feat: fix schema

--- src/student-info/dto/student-info.dto.ts
+++ src/student-info/dto/student-info.dto.ts
@@ -1,76 +1,35 @@
 import { IsBoolean, IsEmail, IsInt, IsString, Length, Max, MaxLength, Min } from "class-validator";
 
 export class StudentInfoDto {
-	@IsString()
-	readonly nickname: string;
-
-	@IsString()
-	readonly firstname: string;
-
-	@IsString()
-	readonly lastname: string;
-
-	@IsInt()
-	@Min(14)
-	@Max(20)
-	readonly age: number;
-
-	@IsString()
-	readonly gender: string;
-
-	@IsString()
-	readonly religion: string;
-
-	@IsString()
-	readonly blood_group: string;
-
-	@IsString()
-	readonly education_level: string;
-
-	@IsString()
-	readonly education_plan: string;
-
-	@IsString()
-	@Length(10, 10)
-	readonly phone_number: string;
-
-	@IsString()
-	readonly medical_insurance: string;
-
-	@IsString()
-	readonly chronic_disease: string;
-
-	@IsString()
-	readonly drug_allergy: string;
-
-	@IsString()
-	readonly food_allergy: string;
-
-	@IsString()
-	readonly address: string;
-
-	@IsBoolean()
-	readonly have_participated: boolean;
-
-	@IsString()
-	readonly shirt_size: string;
-
-	@IsBoolean()
-	readonly have_laptop: boolean;
-
-	@IsString()
-	readonly travel_plan: string;
-
-	@IsString()
-	readonly parent_fullname: string;
-
-	@IsString()
-	readonly parent_relation: string;
-
-	@IsString()
-	@Length(10, 10)
-	readonly parent_phone_number: string;
-
-	@IsBoolean()
-	readonly can_participate_every_day: boolean;
+	std_user_id;
+	std_user;
+	std_user_prefix;
+	std_user_first_name;
+	std_user_last_name;
+	std_user_nick_name;
+	std_info_age;
+	std_info_birthdate;
+	std_info_gender;
+	std_info_religion;
+	std_info_phone_number;
+	std_info_education_level;
+	std_info_education_institute;
+	std_info_education_plan;
+	std_info_parent_fullname;
+	std_info_parent_relation;
+	std_info_parent_phone_number;
+	std_info_have_participated;
+	std_info_have_laptop;
+	std_info_can_participate_every_day;
+	std_info_medical_insurance;
+	std_info_chronic_disease;
+	std_info_drug_allergy;
+	std_info_food_allergy;
+	std_info_blood_group;
+	std_info_address;
+	std_info_shirt_size;
+	std_info_travel_plan;
+	std_info_laptop_os;
+	std_info_have_tablet;
+	std_info_have_mouse;
 }
