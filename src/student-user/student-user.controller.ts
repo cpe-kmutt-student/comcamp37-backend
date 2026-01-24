c306ceb7555095013cbe22454a13712c9eda2679
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-25 02:38:36 +0700

    refactor: remove unuse route

--- src/student-user/student-user.controller.ts
+++ src/student-user/student-user.controller.ts
@@ -8,28 +8,8 @@ import { StudentUserService } from "./student-user.service";
 export class StudentUserController {
 	constructor(private readonly StudentUserService: StudentUserService) {}
 
-	@Get("/me")
-	getMe(@Session() session: UserSession) {
-		return this.StudentUserService.getMe(session.user.id);
-	}
-
-	@Get("/info")
-	getDetails(@Session() session: UserSession) {
-		return this.StudentUserService.getInfo(session.user.id);
-	}
-
-	@Get("/status")
-	getStatus(@Session() session: UserSession) {
-		return this.StudentUserService.getStatus(session.user.id);
-	}
-
-	@Get("/questions")
-	getQuestion(@Session() session: UserSession) {
-		return this.StudentUserService.getQuestions(session.user.id);
-	}
-
-	@Get("/files")
-	getFiles(@Session() session: UserSession) {
-		return this.StudentUserService.getFiles(session.user.id);
+	@Get("/")
+	getUser(@Session() session: UserSession) {
+		return this.StudentUserService.getUser(session.user.id);
 	}
 }
--- src/student-user/student-user.service.ts
+++ src/student-user/student-user.service.ts
@@ -6,42 +6,11 @@ import { PrismaService } from "src/prisma/prisma.service";
 export class StudentUserService {
 	constructor(private readonly prisma: PrismaService) {}
 
-	async getMe(userId: string) {
+	async getUser(userId: string) {
 		return this.prisma.user.findUnique({
 			where: {
 				id: userId,
-			},
-		});
-	}
-
-	async getInfo(userId: string) {
-		return this.prisma.studentInfo.findUnique({
-			where: {
-				std_user_id: userId,
-			},
-		});
-	}
-
-	async getStatus(userId: string) {
-		return this.prisma.studentStatus.findUnique({
-			where: {
-				std_user_id: userId,
-			},
-		});
-	}
-
-	async getQuestions(userId: string) {
-		return this.prisma.studentQuestions.findUnique({
-			where: {
-				std_user_id: userId,
-			},
-		});
-	}
-
-	async getFiles(userId: string) {
-		return this.prisma.studentFiles.findUnique({
-			where: {
-				std_user_id: userId,
+				role: "user",
 			},
 		});
 	}
