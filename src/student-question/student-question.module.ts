17d7f069c4d3673809f6b1a4e93f1ba62aaba5a3
ImJustNon <Im_Just_Non@hotmail.com>
2026-01-28 11:21:53 +0700

    refactor: delete unuse module

--- src/student-question/student-question.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { StudentQuestionController } from "./student-question.controller";
-import { StudentQuestionService } from "./student-question.service";
-
-@Module({
-	controllers: [StudentQuestionController],
-	providers: [StudentQuestionService],
-})
-export class StudentQuestionModule {}
--- src/student-status/student-status.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { StudentStatusController } from "./student-status.controller";
-import { StudentStatusService } from "./student-status.service";
-
-@Module({
-	controllers: [StudentStatusController],
-	providers: [StudentStatusService],
-})
-export class StudentStatusModule {}
