ad3d20bb525f6061a5cae739c3db11fa7724d88a
Your Name <you@example.com>
2026-02-06 14:04:50 +0000

    chore: change traefik to latest version and pull update before start

--- _prod.sh
+++ _prod.sh
@@ -1,5 +1,6 @@
 git pull
 docker compose -f docker-compose.prod.yml down -v
+docker compose -f docker-compose.prod.yml pull
 docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml up -d --scale client=3
 docker compose -f docker-compose.prod.yml logs -f
