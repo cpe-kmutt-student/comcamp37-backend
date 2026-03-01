e462348a34c589a6432b8865a252109b38538474
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-01 23:44:27 +0700

    feat: staff file service

--- src/modules/staff-file/staff-file.module.ts
+++ src/modules/staff-file/staff-file.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { S3Module } from "src/core/s3/s3.module";
 import { StaffFileController } from "./staff-file.controller";
 import { StaffFileService } from "./staff-file.service";
 
 @Module({
 	controllers: [StaffFileController],
 	providers: [StaffFileService],
+	imports: [S3Module],
 })
 export class StaffFileModule {}
