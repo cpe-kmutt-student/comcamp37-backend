c1db21791bccaf102e2e7d91fe4c5d1e2f5e16ba
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-04 02:30:44 +0700

    feat: add get answer service

--- src/modules/staff-academic-question/staff-academic-question.controller.ts
+++ src/modules/staff-academic-question/staff-academic-question.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { AcademicGuard } from "src/common/guards/academic.guard";
+import { RegisGuard } from "src/common/guards/regis.guard";
+import { StaffAcademicQuestionService } from "./staff-academic-question.service";
 
-@Controller("staff-academic-question")
-export class StaffAcademicQuestionController {}
+@Controller("/api/staff/academic/question")
+export class StaffAcademicQuestionController {
+	constructor(private readonly staffAcademicQuestionService: StaffAcademicQuestionService) {}
+
+	@Get("/")
+	@UseGuards(AcademicGuard)
+	getAllRegisAnswer() {
+		return this.staffAcademicQuestionService.getAllAcademicAnswer();
+	}
+
+	@Get("/:id")
+	@UseGuards(AcademicGuard)
+	getRegisAnswerByAppId(@Param("id") appId: string) {
+		return this.staffAcademicQuestionService.getAcademicAnswerByAppId(appId);
+	}
+}
