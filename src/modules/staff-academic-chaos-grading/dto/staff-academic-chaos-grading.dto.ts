6dd968ee5c8f90b321d2692a60d18cb8eaf736be
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-09 04:26:04 +0700

    refactor: set enum staff count  for ident section

--- src/modules/staff-academic-chaos-grading/dto/staff-academic-chaos-grading.dto.ts
+++ src/modules/staff-academic-chaos-grading/dto/staff-academic-chaos-grading.dto.ts
@@ -1,4 +1,15 @@
-import { IsNumber, IsOptional, IsString, IsUUID, Max, Min } from "class-validator";
+import { IsEnum, IsNumber, IsOptional, IsString, IsUUID, Max, Min } from "class-validator";
+
+enum SectionIndentifier {
+	SECTION_1_1 = 11,
+	SECTION_2_1 = 21,
+	SECTION_2_2 = 22,
+	SECTION_2_3 = 23,
+	SECTION_3_1 = 31,
+	SECTION_3_2 = 32,
+	SECTION_3_3 = 33,
+	SECTION_3_4 = 34,
+}
 
 export class StaffAcademicChaosGradingDto {
 	@IsUUID()
@@ -7,10 +18,8 @@ export class StaffAcademicChaosGradingDto {
 	@IsNumber()
 	readonly answer_id: number;
 
-	// @IsNumber()
-	// @Max(2)
-	// @Min(1)
-	// readonly staff_count: number;
+	@IsEnum(SectionIndentifier)
+	readonly staff_count: SectionIndentifier;
 
 	@IsNumber()
 	readonly score: number;
