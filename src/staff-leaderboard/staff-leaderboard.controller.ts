ab90b9819f946b86d2477265f31fdf6df31380cb
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-17 06:10:27 +0700

    refactor: just moveit

--- src/staff-leaderboard/staff-leaderboard.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("staff-leaderboard")
-export class StaffLeaderboardController {}
--- src/staff-leaderboard/staff-leaderboard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { StaffLeaderboardController } from "./staff-leaderboard.controller";
-import { StaffLeaderboardService } from "./staff-leaderboard.service";
-
-@Module({
-	controllers: [StaffLeaderboardController],
-	providers: [StaffLeaderboardService],
-})
-export class StaffLeaderboardModule {}
