f42f45a76a53adbd5764870febd90a4ba1597c20
ImPatipol <122760030+BooMzXI@users.noreply.github.com>
2026-03-20 18:31:58 +0700

    fix: update email template paths and enhance layout styles

--- src/core/email/templates/layout/Header.tsx
+++ src/core/email/templates/layout/Header.tsx
@@ -1,18 +1,13 @@
 import { Img, Section } from "@react-email/components";
 
 const CONFIG = {
-	logourl: "https://res.cloudinary.com/diuembxjq/image/upload/v1769431128/cc37-logo_ipji4s.png",
+	logourl: "https://storage.comcamp.io/web-assets/1.png",
 };
 
 export const Header = () => {
 	return (
-		<Section
-			className="relative overflow-hidden px-12 text-center"
-			style={{
-				background: "linear-gradient(135deg, #1d3071 0%, #004aad 100%)",
-			}}
-		>
-			<Img src={CONFIG.logourl} width="160" height="auto" alt="ComCamp 37 Logo" className="mx-auto relative z-10" />
+		<Section>
+			<Img src={CONFIG.logourl} style={{ width: "auto", height: "100%", maxHeight: "212px" }} alt="ComCamp 37 Logo" className="mx-auto relative z-10" />
 		</Section>
 	);
 };
