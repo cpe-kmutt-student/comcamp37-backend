814cd3dbaae4ad9be90cad763f62b9acc99c45d2
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-24 15:12:32 +0700

    refactor: use staffGuard

--- src/modules/staff-export/staff-export.controller.ts
+++ src/modules/staff-export/staff-export.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Res, UseGuards } from "@nestjs/common";
 import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
 import { type Response } from "express";
 import { RegisGuard } from "src/common/guards/regis.guard";
+import { StaffGuard } from "src/common/guards/staff.guard";
 import { PrismaService } from "src/core/prisma/prisma.service";
 import { StaffExportService } from "./staff-export.service";
 
@@ -10,7 +11,7 @@ export class StaffExportController {
 	constructor(private readonly staffExportService: StaffExportService) {}
 
 	@Get("/")
-	@UseGuards(RegisGuard)
+	@UseGuards(StaffGuard)
 	exportAll(@Res() res: Response) {
 		return this.staffExportService.exportAll(res);
 	}
