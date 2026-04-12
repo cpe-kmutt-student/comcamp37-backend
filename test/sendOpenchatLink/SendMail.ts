6e0c59304cdf556d6daba3ae9abbbdf05cd109bb
ImJustNon <Im_Just_Non@hotmail.com>
2026-04-12 11:05:10 +0700

    test: test send email nong nong

--- test/sendOpenchatLink/SendMail.ts
+++ test/sendOpenchatLink/SendMail.ts
@@ -2,14 +2,15 @@ import fs from "node:fs";
 import path from "node:path";
 import { render } from "@react-email/render";
 import AnnouncementEmail from "src/core/email/templates/AnnouncementEmail";
+import SendLinkOpenChat from "src/core/email/templates/SendLineOpenChat";
 import { MailConnector } from "test/mail/MailConnector";
 import { PrismaConnector } from "test/PrismaConnector";
 
 class SendMail extends MailConnector {
-	async sendAnnouncement(email: string, name: string) {
+	async sendAnnouncement(email: string, nick: string, first: string, last: string) {
 		try {
-			const html = await render(AnnouncementEmail({ name }));
-			return await this.sendMail(email, "ประกาศผลการคัดเลือก ComCamp 37", html);
+			const html = await render(SendLinkOpenChat({ nickname: nick, firstname: first, lastname: last }));
+			return await this.sendMail(email, "LINE OpenChat สำหรับผู้เข้าร่วมค่าย ComCamp 37", html);
 		} catch (error) {
 			this.handleError("announcement", email, error);
 		}
@@ -27,20 +28,20 @@ class SendMail extends MailConnector {
 
 		for (const { email, nickname, firstname, lastname } of emails) {
 			try {
-				await this.sendAnnouncement(email, nickname);
+				await this.sendAnnouncement(email, nickname, firstname, lastname);
 
 				const readlast = fs.readFileSync(path.join(__dirname, "./log/success.json"), "utf8");
 				const toJson: string[] = JSON.parse(readlast);
 				toJson.push(email);
 				fs.writeFileSync(path.join(__dirname, "./log/success.json"), JSON.stringify(toJson, null, 2), "utf8");
-				console.log(`Success : ${name} - ${email}`);
+				console.log(`Success : ${firstname} - ${email}`);
 			} catch (e) {
 				const readlast = fs.readFileSync(path.join(__dirname, "./log/error.json"), "utf8");
 				const toJson: string[] = JSON.parse(readlast);
 				toJson.push(email);
 				fs.writeFileSync(path.join(__dirname, "./log/error.json"), JSON.stringify(toJson, null, 2), "utf8");
 
-				console.log(`Send Email Error: ${name} - ${email}`, e);
+				console.log(`Send Email Error: ${firstname} - ${email}`, e);
 			}
 		}
 	}
