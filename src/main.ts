4f2b5755499bf94a72d11343f31c92e991f7f571
ImJustNon <Im_Just_Non@hotmail.com>
2026-06-11 22:59:03 +0700

    feat: terminal log logo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import fs from "node:fs";
 import type { IncomingMessage, ServerResponse } from "node:http";
+import path from "node:path";
 import { ValidationPipe } from "@nestjs/common";
 import type { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 import { NestFactory } from "@nestjs/core";
@@ -6,6 +8,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { apiReference } from "@scalar/nestjs-api-reference";
 import { toNodeHandler } from "better-auth/node";
 import bodyParser from "body-parser";
+import chalk from "chalk";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import { AppModule } from "./app.module";
@@ -42,6 +45,7 @@ async function bootstrap() {
 	await app
 		.listen(config.app.port)
 		.then(() => {
+			helloComCamp();
 			logger.start("Service Port :", config.app.port);
 			logger.info("Cors Origin :", config.app.allowOrigins.join(" "));
 			logger.info("Frontend URL :", config.app.frontendUrl);
@@ -55,5 +59,25 @@ async function bootstrap() {
 		.catch((e) => {
 			logger.error(e);
 		});
+}
+
+function helloComCamp() {
+	if (fs.existsSync(path.join(process.cwd(), "comcamp37.txt"))) {
+		try {
+			const logFileData = fs.readFileSync(path.join(process.cwd(), "comcamp37.txt"), "utf8");
+			function colorize(text: string) {
+				return text
+					.replace(/@/g, chalk.white("@"))
+					.replace(/#/g, chalk.yellow("#"))
+					.replace(/\+/g, chalk.hex("#ff7a45")("+"))
+					.replace(/\*/g, chalk.yellow("*"))
+					.replace(/%/g, chalk.hex("#ff9c66")("%"))
+					.replace(/=/g, chalk.gray("="))
+					.replace(/-/g, chalk.gray("-"));
+			}
+
+			console.log(colorize(logFileData));
+		} catch (e) {}
+	}
 }
 bootstrap();
